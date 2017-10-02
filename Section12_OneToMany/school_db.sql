42945690f74f893349e741195487ddc0c19e973f
Blanka Kovács <kovacsblanka06@gmail.com>
2017-10-02 08:48:14 +0200

    Create school_db.sql

---
+++ Section12_OneToMany/school_db.sql
@@ -0,0 +1,72 @@
+DROP database school_db;
+CREATE database school_db;
+USE school_db;
+
+CREATE TABLE students (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(100)
+);
+
+CREATE TABLE papers (
+  title VARCHAR(100),
+  grade INT,
+  student_id INT, 
+  FOREIGN KEY(student_id) REFERENCES students(id)
+);
+
+INSERT INTO students (first_name) VALUES 
+('Caleb'), ('Samantha'), ('Raj'), ('Carlos'), ('Lisa');
+
+INSERT INTO papers (student_id, title, grade ) VALUES
+(1, 'My First Book Report', 60),
+(1, 'My Second Book Report', 75),
+(2, 'Russian Lit Through The Ages', 94),
+(2, 'De Montaigne and The Art of The Essay', 98),
+(4, 'Borges and Magical Realism', 89);
+
+-- challenge 1: get name, title of paper and corresponding grade
+
+SELECT first_name, title, grade FROM students
+JOIN papers
+  ON students.id = papers.student_id
+ORDER BY grade DESC;
+
+-- challenge 2: get name, title of paper and corresponding grade (incl. NULL)
+
+SELECT first_name, title, grade FROM students
+LEFT JOIN papers
+  ON students.id = papers.student_id;
+  
+-- challenge 3: get name, title of paper and corresponding grade ("MISSING" title, NULL->0)
+
+SELECT
+  first_name,
+  IFNULL(title, "MISSING") as title,
+  IFNULL(grade, 0) as grade
+FROM students
+LEFT JOIN papers
+  ON students.id = papers.student_id;
+
+-- challenge 4: get name and avg grade
+
+SELECT first_name, IFNULL(AVG(grade), 0) as average FROM students
+LEFT JOIN papers
+  ON students.id = papers.student_id
+GROUP BY students.id
+ORDER BY average DESC;
+
+-- challenge 5: get name, avg and passing_status (75=< "PASSING", else "FAILING")
+
+SELECT
+  first_name, 
+  IFNULL(AVG(grade), 0) as average, 
+  CASE
+    WHEN AVG(grade) IS NULL THEN "FAILING"
+    WHEN AVG(grade) >= 75 THEN "PASSING"
+    ELSE "FAILING"
+  END AS passing_status
+FROM students
+LEFT JOIN papers
+  ON students.id = papers.student_id
+GROUP BY students.id
+ORDER BY average DESC;
